b74681a2e882230167513988672a717d33dc0208
shomyich <220009@astanait.edu.kz>
2023-05-14 15:23:38 +0600

    hash(K key) - Hash function to calculate the index of the key

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -16,18 +16,28 @@ public class MyHashTable<K, V> {
         }
     }
 
-    private HashNode<K, V> Object[];
+    private HashNode<K, V>[] chainArray; // or Object[]
+    private int M = 11; // default number of chains
     private int size;
 
     public MyHashTable() {
-
+        chainArray = new HashNode[M];
+        size = 0;
     }
 
     public MyHashTable(int M) {
+        this.M = M;
+        chainArray = new HashNode[M];
+        size = 0;
     }
 
     private int hash(K key) {
+        // Hash function to calculate the index of the key
+        int hashCode = key.hashCode();
+        int index = Math.abs(hashCode % M);
+        return index;
     }
+
 
     public void put(K key, V value) {
     }
